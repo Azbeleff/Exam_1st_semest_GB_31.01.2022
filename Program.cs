25d780114e67edfdac5b3b60d4d79fd03d49d320
Nick <you@example.com>
2022-01-26 13:17:15 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// **Задача**: 
+
+
+void PrintArray(string[] newarray)
+{
+    for (int i = 0; i < newarray.Length; i++)
+    {
+        Console.Write($"{newarray[i]} ");
+    }
+}
+
+int newarraysize(string[] newarray)
+{
+    int targetarraylength = 0;
+    for (int i = 0; i < newarray.Length; i++)
+    {
+        if (newarray[i].Length <= 3)
+        {
+            targetarraylength++;
+        }
+    }
+    return targetarraylength;
+}
+
+void Fills3array(string[] newarray, string[] targetarray)
+{
+    for (int j = 0, k = 0; j < newarray.Length; j++)
+    {
+        if (newarray[j].Length <= 3)
+        {
+            targetarray[k] = newarray[j];
+            k++;
+        }
+    }
+}
+
+void Demo ()
+{
+    string[] countries = {"Russia" , "UAE", "USA", "UK", "Germany", "France", "Italy", "Sweden"};
+    //PrintArray(countries);
+    Console.WriteLine("");
+    string[] countriesshort = new string [newarraysize(countries)];
+    Fills3array (countries, countriesshort);
+
+    PrintArray(countriesshort);
+}
+
+Demo ();
